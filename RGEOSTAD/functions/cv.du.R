ac5cb4e805ceabec3bbc63a3585210760365b685
Earth-Science Modeling Group <esmg.mx@gmail.com>
2022-03-31 19:33:25 -0600

    Delete cv.du.R

--- RGEOSTAD/functions/cv.du.R
+++
@@ -1,6 +0,0 @@
-cv.du <- function(u,v) (dim(matriz.copem)[1]-1)*sum(matriz.copem*
-((dbinom(-1:(dim(matriz.copem)[1]-2),dim(matriz.copem)[1]-2,u) -
-dbinom(0:(dim(matriz.copem)[1]-1),dim(matriz.copem)[1]-2,u)*
-c(-1,rep(1,dim(matriz.copem)[1]-1)))%*%t(dbinom(0:(dim(matriz.copem)[
-1]-1),dim(matriz.copem)[1]-1,v))))
-
