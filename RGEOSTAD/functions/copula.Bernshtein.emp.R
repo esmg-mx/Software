13a6dd1c7bfd7e98e5e4b2377a516f0c8e8273df
Earth-Science Modeling Group <esmg.mx@gmail.com>
2022-03-31 19:31:50 -0600

    Delete copula.Bernshtein.emp.R

--- RGEOSTAD/functions/copula.Bernshtein.emp.R
+++
@@ -1,4 +0,0 @@
-copula.Bernshtein.emp <- function(u,v) sum(matriz.copem*
-(dbinom(0:(dim(matriz.copem)[1]-1),dim(matriz.copem)[1]-1,u)%*%
-t(dbinom(0:(dim(matriz.copem)[1]-1),dim(matriz.copem)[1]-1,v))))
-
